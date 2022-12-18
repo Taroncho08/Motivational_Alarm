a9f36464952d57d1697e288197bb05f3292c37d3
TaronCode <taron.khachinyan@gmail.com>
2022-12-18 22:51:02 +0400

    All project in ESP

--- motivatio_alarm/motivatio_alarm.ino
+++ motivatio_alarm/motivatio_alarm.ino
@@ -1,14 +1,14 @@
 #include <EEPROM.h>
-#include <GyverTM1637.h>
 #include <GyverEncoder.h>
 #include <RTClib.h>
 #include "BluetoothSerial.h"
 #include <Wire.h>
+#include <GyverTM1637.h>
 
 #define CLK 4
 #define DIO 5
 
-
+char x;
 bool istime = false;
 int clickCount = 0;
 int ListIndex = 0;
@@ -18,12 +18,15 @@ int alarmList[50];
 int testVal = 0;
 uint32_t tmr;
 int address = 0;
+bool isInList = false;
+int testArr[2] = {};
+int testArrIndex = 0;
 
 
 
 RTC_DS3231 rtc;
 GyverTM1637 disp(CLK, DIO);
-Encoder enc(11, 10, 9);
+Encoder enc(35, 34, 32);
 BluetoothSerial BTSerial;
 
 
@@ -49,7 +52,7 @@ void setup() {
     EEPROM.get(1, alarmList);
     EEPROM.get(2, ListIndex);
   }
-  
+
  
 }
 void loop(){
@@ -69,7 +72,7 @@ void loop(){
        
         for(int i = 0; i<ListIndex; i++){
           if(i % 2 == 0){
-          BTSerial.print(String(arr2[i]) + ":" + String(arr2[i+1]));
+          BTSerial.print(String(alarmList[i]) + ":" + String(alarmList[i+1]));
           delay(100);
           }
           //BTSerial.print(arr[i][1]);    
@@ -78,11 +81,26 @@ void loop(){
    
     
     else{
-        
-        arr2[ListIndex] = x;
-        ListIndex++;
-
+      testArr[testArrIndex] = x;
+      if (testArrIndex == 1){
+        for(int d = 0; d < ListIndex; d++){
+          if(d % 2 == 0){
+              if (testArr[0] == alarmList[d] and testArr[1] == alarmList[d+1]){
+                  isInList = true;
+                }
+              }
+        }
+      
+      if (isInList == false){
+        alarmList[ListIndex] = testArr[0];
+        alarmList[ListIndex + 1] = testArr[1];
+        ListIndex+=2;
       }
+      else{
+          isInList == false;
+        }
+      }
+    }
       
   }
 
@@ -96,7 +114,7 @@ void loop(){
 
    if (clickCount == 1){
       
-      disp.displayClock(hrs, time.minutes);
+      disp.displayClock(hrs, now.minute());
       if (enc.isRight()){
           hrs++;
         }
@@ -125,6 +143,14 @@ void loop(){
       if (mins < 0)mins = 59;
     }
    else if (clickCount == 3){
+    for(int c = 0; c < ListIndex; c++){
+      if(c % 2 == 0){
+          if (hrs == alarmList[c] and mins == alarmList[c+1]){
+            clickCount = 0;
+            }
+          }
+    }
+    if(clickCount == 3){
       alarmList[ListIndex] = hrs;
       alarmList[ListIndex + 1] = mins;
       ListIndex+=2;
@@ -136,8 +162,9 @@ void loop(){
       EEPROM.put(2, ListIndex);
       clickCount = 0;
     }
+    }
    else{
-        disp.displayClock(time.Hours, time.minutes);
+        disp.displayClock(now.hour(), now.minute());
         
     }
   
@@ -165,17 +192,19 @@ void loop(){
       
     }        
 
-if (istime == false){    
-  Serial.print(now.hour(), DEC);    // Час
-  Serial.print(':');
-  Serial.println(now.minute(), DEC);
-}
+//if (istime == false){    
+//  Serial.print(now.hour(), DEC);    // Час
+//  Serial.print(':');
+//  Serial.println(now.minute(), DEC);
+//}
 
 
 //Serial.println(testVal);
 //Serial.println(ListIndex);
 
-    
+for(int r = 0; r < ListIndex; r++){
+    Serial.println(alarmList[r]);
+  }
     
 
   
